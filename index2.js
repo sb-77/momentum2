3f27623b368b05d543529d5764d43f4c147850bd
이수빈 <105586878+sb-77@users.noreply.github.com>
2022-11-17 06:39:05 +0900

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,27 @@
+// 자바스크립트 파일만 따로 떼어낸 형태
+$(document).ready(function () {
+    renderCurrentTime();
+    renderQuote();
+});
+
+
+function renderCurrentTime() {
+let url = `https://worldtimeapi.org/api/timezone/Asia/Seoul`;
+fetch(url)
+.then(res => res.json()).then((data) => {
+    let datetime = data['datetime'].substr(11,5);
+    $('#time').text(datetime);
+});
+}
+
+
+function renderQuote() {
+    let url = `https://api.quotable.io/random`;
+    fetch(url)
+        .then(res => res.json()).then((data) => {
+            let content = `" ${data['content']} "`;
+            let author = `- ${data['author']} -`;
+            $('#content').text(content);
+            $('#author').text(author);
+        });
+}
